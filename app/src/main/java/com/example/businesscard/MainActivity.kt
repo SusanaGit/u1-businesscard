1633504e640abcfd8ddde6706b2a4a4b3fd01d3e
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-09-30 18:36:27 +0200

    change color to blue

--- app/src/main/java/com/example/businesscard/MainActivity.kt
+++ app/src/main/java/com/example/businesscard/MainActivity.kt
@@ -24,9 +24,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.modifier.modifierLocalOf
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -66,57 +68,10 @@ private fun BusinessCard() {
 }
 
 @Composable
-private fun ContactInformation() {
-    Column(
-        modifier = Modifier
-            .padding(top = 200.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Row(
-            modifier = Modifier
-                .padding(10.dp)
-        ) {
-            Icon(
-                imageVector = Icons.Default.Phone,
-                contentDescription = stringResource(R.string.telf)
-            )
-            Text(
-                text = stringResource(R.string.telf)
-            )
-        }
-        Row(
-            modifier = Modifier
-                .padding(10.dp)
-        ) {
-            Icon(
-                imageVector = Icons.Default.AccountCircle,
-                contentDescription = stringResource(R.string.susanagit)
-            )
-            Text(
-                text = stringResource(R.string.susanagit)
-            )
-
-        }
-        Row(
-            modifier = Modifier
-                .padding(10.dp)
-        ) {
-            Icon(
-                imageVector = Icons.Default.Email,
-                contentDescription = stringResource(R.string.mail)
-            )
-            Text(
-                text = stringResource(R.string.mail)
-            )
-        }
-    }
-}
-
-@Composable
 private fun CenterInformation() {
     Column(
         modifier = Modifier
-            .padding(top = 200.dp),
+            .padding(top = 250.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Row(){
@@ -131,7 +86,8 @@ private fun CenterInformation() {
         Row(){
             Text(
                 text = stringResource(R.string.manekineko),
-                fontSize = 20.sp
+                fontSize = 30.sp,
+                color = Color(0xFF66ADE6)
             )
         }
     }
@@ -147,6 +103,56 @@ private fun Logo() {
             .height(150.dp)
             .width(150.dp)
     )
+}
+
+@Composable
+private fun ContactInformation() {
+    Column(
+        modifier = Modifier
+            .padding(top = 150.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Row(
+            modifier = Modifier
+                .padding(10.dp)
+        ) {
+            Icon(
+                imageVector = Icons.Default.Phone,
+                contentDescription = stringResource(R.string.telf),
+                tint = Color(0xFF66ADE6)
+            )
+            Text(
+                text = stringResource(R.string.telf)
+            )
+        }
+        Row(
+            modifier = Modifier
+                .padding(10.dp)
+        ) {
+            Icon(
+                imageVector = Icons.Default.AccountCircle,
+                contentDescription = stringResource(R.string.susanagit),
+                tint = Color(0xFF66ADE6)
+            )
+            Text(
+                text = stringResource(R.string.susanagit)
+            )
+
+        }
+        Row(
+            modifier = Modifier
+                .padding(10.dp)
+        ) {
+            Icon(
+                imageVector = Icons.Default.Email,
+                contentDescription = stringResource(R.string.mail),
+                tint = Color(0xFF66ADE6)
+            )
+            Text(
+                text = stringResource(R.string.mail)
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
