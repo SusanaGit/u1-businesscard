02d9f2e60e46f3619a2809f17bdb324d0ca425b1
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-09-30 18:03:46 +0200

    center information finished

--- app/src/main/java/com/example/businesscard/MainActivity.kt
+++ app/src/main/java/com/example/businesscard/MainActivity.kt
@@ -5,22 +5,25 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.businesscard.ui.theme.BusinessCardTheme
 
 class MainActivity : ComponentActivity() {
@@ -47,19 +50,38 @@ private fun BusinessCard() {
             .fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Box(
-            modifier = Modifier
-                .padding(top = 200.dp)
-        ) {
+        Row() {
             CenterInformation()
         }
+        Row() {
 
+        }
     }
 }
 
 @Composable
 private fun CenterInformation() {
-    Logo()
+    Column(
+        modifier = Modifier
+            .padding(top = 200.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Row(){
+            Logo()
+        }
+        Row(){
+            Text(
+                text = stringResource(R.string.susana_figueroa),
+                fontSize = 35.sp
+            )
+        }
+        Row(){
+            Text(
+                text = stringResource(R.string.manekineko),
+                fontSize = 20.sp
+            )
+        }
+    }
 }
 
 @Composable
@@ -69,8 +91,8 @@ private fun Logo() {
         painter = logo,
         contentDescription = "logo",
         modifier = Modifier
-            .height(200.dp)
-            .width(200.dp)
+            .height(150.dp)
+            .width(150.dp)
     )
 }
 
