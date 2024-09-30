f2209339049bbebd1c8cf56025198e9769705668
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-09-30 17:04:45 +0200

    add logo

--- app/src/main/java/com/example/businesscard/MainActivity.kt
+++ app/src/main/java/com/example/businesscard/MainActivity.kt
@@ -4,12 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.businesscard.ui.theme.BusinessCardTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,13 +40,38 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 private fun BusinessCard() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        Box() {
+            CenterInformation()
+        }
+    }
+}
 
+@Composable
+private fun CenterInformation(
+) {
+    Logo()
+}
+
+@Composable
+private fun Logo() {
+    val logo = painterResource(R.drawable.logo)
+    Image(
+        painter = logo,
+        contentDescription = "logo",
+        modifier = Modifier
+            .height(200.dp)
+            .width(200.dp)
+    )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun BusinessCardPreview() {
     BusinessCardTheme {
-
+        BusinessCard()
     }
 }
